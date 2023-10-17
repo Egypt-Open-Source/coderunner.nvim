c41ecaed7b411dce553aab84d6cb9193c981fa04
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-10-17 13:04:48 +0300

    introduce oop

--- lua/coderunner/init.lua
+++ lua/coderunner/init.lua
@@ -1,18 +1,18 @@
-local M = {}
+local CodeRunner = {}
 
-local cpp = function(file)
+function CodeRunner:cpp(file)
     local command = ':call jobsend(b:terminal_job_id, "g++ ' .. file .. '\\n")'
     vim.cmd(command)
     local runcomm = ':call jobsend(b:terminal_job_id, "./a.out \\n")'
     vim.cmd(runcomm)
 end
 
-local py = function(file)
+function CodeRunner:py(file)
     local command = ':call jobsend(b:terminal_job_id, "python ' .. file .. '\\n")'
     vim.cmd(command)
 end
 
-M.run = function()
+function CodeRunner:run()
     local file = vim.fn.expand("%:p")
     local extension = file:match("([^.]+)$")
 
@@ -21,16 +21,16 @@ M.run = function()
         print("CodeRunner: Error: Not supported file type")
     elseif extension then
         local small_extension = extension:lower()
-        if small_extension == "cpp" or small_extension == "c" then
+        if small_extension == ("cpp" or "c") then
             vim.cmd("split | terminal")
-            cpp(file)
+            self:cpp(file)
         elseif small_extension == "py" then
             vim.cmd("split | terminal")
-            py(file)
+            self:py(file)
         else
             print("CodeRunner: Error: Not supported file type")
         end
     end
 end
 
-return M
+return CodeRunner
